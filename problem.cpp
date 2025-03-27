619a094ef30385340cd45d393c93c793762789d0
Apurbo73 <apurbodebnath895@gmail.com>
2025-03-27 09:43:20 +0600

    done

---
+++ problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  while(n--){
+      int a,b,c,d=0,f=0;
+      cin>>a>>b>>c;
+      d=a*5+b*10;
+      f=d/c;
+      cout<<f<<endl;
+  }
+
+}
